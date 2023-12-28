0c717597c1121ea108627c700efbe80a2ff63f50
dev20223 <chingizbekturdiboyev@gmail.com>
2023-12-28 18:12:33 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// 1-masala
+
+
+// 2-masala
+// function countStr(str) {
+//     var vowels = ['a', 'e', 'i', 'o', 'u'];
+//     var count = 0;
+    
+//     for (var i = 0; i < str.length; i++) {
+//       var char = str[i].toLowerCase();
+//       if (vowels.includes(char)) {
+//         count++;
+//       }
+//     }
+    
+//     return count;
+//   }
+  
+//   var input = "Radar";
+//   var strCount = countStr(input);
+  
+//   console.log(strCount);
+  
+
+// 3-masala
+
+// function palindrom(str) {
+//     let reverseStr = str.split('').reverse().join('');
+//     return str == reverseStr;
+//     }
+    
+//     console.log(palindrom("radar"));
+
+// 4-masala
+
+
+// 5-masala
+
+
+// 6-masala
